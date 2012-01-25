0008f1cf73b917a2c1eb786d102af7f2f9c408df
Matthew Riley <matthew@matthewriley.name>
2012-01-25 21:50:54 +1100

    refactor regular expressions

--- rakelib/ruby_naming_convention.rb
+++ rakelib/ruby_naming_convention.rb
@@ -1,25 +1,25 @@
 class RubyNamingConvention
 
   def module_name text
-    class_name text
+    words(text).join
   end
 
   def class_name text
-    text.gsub(/[\W_]/, ' ').gsub(/[A-Z]/, ' \0').strip.split(' ').map { |s| s.capitalize }.join
+    module_name text
   end
 
   def method_name text
-    text.gsub(/[\W_]/, ' ').gsub(/[A-Z]{2,}/) { |m| m.downcase }.gsub(/[A-Z]/, ' \0').strip.split(' ').join('_').downcase
+    words(text).join('_').downcase
   end
 
   def arg_name text
-    ruby_case(text)
+    method_name text
   end
 
   private
 
-  def ruby_case value
-    value.gsub(/[A-Z]/, ' \0').strip.gsub(' ', '_').downcase
+  def words text
+    text.gsub(/[\W_]/, ' ').gsub(/[A-Z*]/, ' \0').split(' ').map { |s| s.capitalize }
   end
 
 end
