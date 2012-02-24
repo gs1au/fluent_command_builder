9a5e7d53b5ad4c068e8cf6efeba9c36ebf768313
Matthew Riley <matthew@matthewriley.name>
2012-02-24 21:56:47 +1100

    refactor to simplify creation of command

--- code_generator/command.rb
+++ code_generator/command.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+require_relative 'code_generator'
 require_relative 'node'
 
 module CodeGenerator
@@ -5,13 +7,43 @@ module CodeGenerator
 
     attr_reader :command_name
 
-    def initialize raw_text
-      @command_name = raw_text.match(/^([-\w]+)/)[1]
-      super raw_text[@command_name.length, raw_text.length]
+    def initialize command_text
+      hash = command_hash command_text
+      command_line = hash.keys[0]
+      @command_name = command_line.match(/^([-\w]+)/)[1]
+      command_line_without_name = command_line[@command_name.length, command_line.length]
+      super command_line_without_name
+      process_array hash.values[0], self
     end
 
     def node_name
       @node_name ||= node_alias || @command_name
+    end
+
+    private
+
+    def command_hash command_text
+      array = YAML.load command_yaml(command_text)
+      array[0]
+    end
+
+    def command_yaml command_text
+      command_text.split(NEW_LINE).map { |l| l.gsub(/^( *)(\S.+)$/, '\1- "\2":') }.join NEW_LINE
+    end
+
+    def process_array array, parent_node=nil
+      array ||= []
+      array.each do |hash|
+        process_hash hash, parent_node
+      end
+    end
+
+    def process_hash hash, parent_node
+      hash.each_pair do |node_text, child_nodes|
+        node = Node.new " #{node_text}" # todo: remove need for leading space
+        parent_node.child_nodes << node
+        process_array child_nodes, node
+      end
     end
 
   end
--- code_generator/command_definition.rb
+++ code_generator/command_definition.rb
@@ -1,44 +1,45 @@
-require 'yaml'
 require_relative 'command'
-require_relative 'command_definition_text'
-require_relative 'node'
 
 module CodeGenerator
   class CommandDefinition
 
-    attr_reader :versions
+    def initialize stream
+      text = stream.read
+      @lines = text.split NEW_LINE
+    end
 
-    def initialize versions, yaml
-      @versions = versions
-      @array = YAML.load yaml
+    def versions
+      @versions ||= @lines[0].split(',').map { |v| v.strip }
     end
 
     def command
-      @command ||= load_command
+      @command ||= Command.new command_text
     end
 
     private
 
-    def load_command
-      hash = @array[0]
-      command = Command.new hash.keys[0]
-      process_array hash.values[0], command
-      command
+    def command_text
+      lines = expand_options
+      end_index = option_references_line_index ? option_references_line_index - 1 : lines.length - 1
+      lines[1..end_index].join NEW_LINE
     end
 
-    def process_array array, parent_node=nil
-      array ||= []
-      array.each do |hash|
-        process_hash hash, parent_node
+    def expand_options
+      @lines.map do |line|
+        option_matches = option_references.select { |o| o if o.start_with? line.strip }
+        option_matches.empty? ? line : (line[/^ +/].to_s + option_matches[0])
       end
     end
 
-    def process_hash hash, parent_node
-      hash.each_pair do |node_text, child_nodes|
-        node = Node.new " #{node_text}" # todo: remove need for leading space
-        parent_node.child_nodes << node
-        process_array child_nodes, node
-      end
+    def option_references
+      return [] unless option_references_line_index
+      start_index = option_references_line_index + 1
+      end_index = @lines.length - 1
+      @lines[start_index..end_index].map { |l| l.strip }
+    end
+
+    def option_references_line_index
+      @option_references_line_index ||= @lines.index 'options:'
     end
 
   end
