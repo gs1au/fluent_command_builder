fa2524e189b350e49e05250187a9f707c5e34723
Matthew Riley <matthew@matthewriley.name>
2012-05-25 21:25:36 +1000

    use brackets around method args

--- lib/fluent_command_builder/command_builder.rb
+++ lib/fluent_command_builder/command_builder.rb
@@ -5,13 +5,13 @@ module FluentCommandBuilder
 
     attr_accessor :command_name, :path
 
-    def initialize command_name, path=nil
+    def initialize(command_name, path=nil)
       @command_name = command_name
       @path = path
       @args = ''
     end
 
-    def format value, delimiter=nil, key_value_separator=nil
+    def format(value, delimiter=nil, key_value_separator=nil)
 
       if value.kind_of? Array and delimiter.nil?
         return array_to_s(value.map { |v| quote_if_includes_space v }, ' ')
@@ -33,11 +33,11 @@ module FluentCommandBuilder
       quote_if_includes_space value_as_string
     end
 
-    def append value
+    def append(value)
       @args << value.to_s
     end
 
-    def append_arg arg
+    def append_arg(arg)
       append " #{arg.to_s}"
     end
 
@@ -57,15 +57,15 @@ module FluentCommandBuilder
       executable
     end
 
-    def hash_to_array hash, key_value_separator
+    def hash_to_array(hash, key_value_separator)
       hash.map { |k, v| k.to_s + key_value_separator + v.to_s }
     end
 
-    def array_to_s array, delimiter
+    def array_to_s(array, delimiter)
       array.join delimiter
     end
 
-    def quote_if_includes_space value
+    def quote_if_includes_space(value)
       value.to_s.include?(' ') ? %Q["#{value}"] : value
     end
 
