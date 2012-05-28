4b1e3d40e457331993746c9476bd9351becd4b8e
Matthew Riley <matthew@matthewriley.name>
2012-05-28 20:05:01 +1000

    added unit tests for rake

---
+++ test/commands/rake_09_test.rb
@@ -0,0 +1,19 @@
+require 'test/unit'
+require_relative '../../lib/fluent_command_builder'
+include FluentCommandBuilder
+
+class Rake09Test < Test::Unit::TestCase
+  
+  def test_full_command
+    expected = 'rake[ task] --classic-namespace --describe pattern --dry-run --execute code --execute-print code --execute-continue code --libdir libDir --no-search --prereqs --quiet --rakefile file --rakelibdir rakeLibDir --require file --rules --silent --no-system --tasks pattern --trace --verbose --version --where pattern --no-deprecation-warnings --help'
+    actual = rake_09('task').classic_namespace.describe('pattern').dry_run.execute('code').execute_print('code').execute_continue('code').libdir('libdir').no_search.prereqs.quiet.rakefile('file').rakelibdir('rakelibdir').require('file').rules.silent.no_system.tasks('pattern').trace.verbose.version.where('pattern').no_deprecation_warnings.help.to_s
+    assert_equal expected, actual
+  end
+  
+  def test_task_is_optional
+    expected = 'rake'
+    actual = rake_09.to_s
+    assert_equal expected, actual
+  end
+  
+end
