3fa08b743ba739887031edb43759a0c6a33a4efe
Matthew Riley <matthew@matthewriley.name>
2012-05-28 23:42:34 +1000

    add unit tests for installutil 1.0

---
+++ test/commands/installutil_10_test.rb
@@ -0,0 +1,25 @@
+# Note: Tests do not represent valid command line usage.
+
+require_relative '../lib/command_test_base'
+
+class InstallUtil10Test < CommandTestBase
+
+  def test_full_command
+    expected = 'installUtil /help assemblyPath /logFile=fileName /logToConsole=bool /showCallStack /uninstall assemblyFileName'
+    actual = installutil_11.help('assemblyPath').log_file('fileName').log_to_console('bool').show_call_stack.uninstall.assembly_file_name('assemblyFileName')
+    assert_command expected, actual
+  end
+
+  def test_help_assembly_path_is_optional
+    expected = 'installUtil /help'
+    actual = installutil_11.help
+    assert_command expected, actual
+  end
+
+  def test_log_file_file_name_is_optional
+    expected = 'installUtil /logFile'
+    actual = installutil_11.log_file
+    assert_command expected, actual
+  end
+
+end
