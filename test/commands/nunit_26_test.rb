ab1fee79143bca1c17c9e6f8a20132bb9134667d
Matthew Riley <matthew@matthewriley.name>
2012-05-28 20:00:13 +1000

    added unit tests for nunit

---
+++ test/commands/nunit_26_test.rb
@@ -0,0 +1,14 @@
+require 'test/unit'
+require_relative '../../lib/fluent_command_builder'
+include FluentCommandBuilder
+
+class NUnit26Test < Test::Unit::TestCase
+
+  def test_full_command
+    expected = 'nunit-console inputFiles /fixture:fixture /load:fixture /run:test /runList:file /config:config /result:file /xml:file /xmlConsole /noResult /noXml /output:file /out:file /err:file /work:directory /labels /trace:level /include:category /exclude:category /framework:framework /process:process /domain:domain /apartment:apartment /noShadow /noThread /timeout:timeout /wait /noLogo /noDots /stopOnError /cleanup /help'
+    actual = nunit_26('inputFiles').fixture('fixture').load('fixture').run('test').run_list('file').config('config').result('file').xml('file').xml_console.no_result.no_xml.output('file').out('file').err('file').work('directory').labels.trace('level').include('category').exclude('category').framework('framework').process('process').domain('domain').apartment('apartment').no_shadow.no_thread.timeout('timeout').wait.no_logo.no_dots.stop_on_error.cleanup.help.to_s
+    assert_equal expected, actual
+  end
+  
+end
+
