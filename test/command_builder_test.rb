a67da79e32f11ee5c5c961ced5a16b6556e6d049
Matthew Riley <matthew@matthewriley.name>
2012-06-02 22:25:10 +1000

    added and refactored command builder tests

--- test/command_builder_test.rb
+++ test/command_builder_test.rb
@@ -4,26 +4,38 @@ include FluentCommandBuilder
 
 class CommandBuilderTest < Test::Unit::TestCase
 
-  def test_should_initialize_with_existing_command
-    builder = CommandBuilder.new 'MSBuild'
-    assert_equal 'MSBuild', builder.to_s
+  def test_initialize_with_existing_command
+    builder = CommandBuilder.new 'commandName'
+    assert_equal 'commandName', builder.to_s
   end
 
-  def test_should_format_array_using_delimiter
-    builder = CommandBuilder.new 'MSBuild'
-    value = builder.format(['a', 'b', 'c'], ';')
+  def test_format_array_using_delimiter
+    builder = CommandBuilder.new 'commandName'
+    value = builder.format %w(a b c), ';'
     assert_equal 'a;b;c', value
   end
 
-  def test_should_format_hash_using_key_value_separator_and_delimiter
-    builder = CommandBuilder.new 'MSBuild'
-    value = builder.format({:a => '1', :b => 2, :c => 3}, ';', '=')
+  def test_format_hash_using_key_value_separator_and_delimiter
+    builder = CommandBuilder.new 'commandName'
+    value = builder.format({'a' => '1', 'b' => 2, 'c' => 3}, ';', '=')
     assert_equal 'a=1;b=2;c=3', value
   end
 
-  def test_should_not_contain_trailing_space
-    builder = CommandBuilder.new 'MSBuild'
-    builder.append('/property:a=1')
+  def test_format_plain_string_is_not_altered
+    builder = CommandBuilder.new 'commandName'
+    value = builder.format 'plainString'
+    assert_equal 'plainString', value
+  end
+
+  def test_format_command_base_object
+    builder = CommandBuilder.new 'commandName'
+    value = builder.format CommandBase.new(CommandBuilder.new 'subCommandName')
+    assert_equal 'subCommandName', value
+  end
+
+  def test_should_not_append_trailing_space
+    builder = CommandBuilder.new 'commandName'
+    builder.append 'optionName'
     assert_no_match /\s$/, builder.to_s
   end
 
