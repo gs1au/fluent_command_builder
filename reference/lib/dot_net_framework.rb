7624bd71a8de6182959fed91e87f1168cb74282d
Matthew Riley <matthew@matthewriley.name>
2012-07-24 23:01:10 +1000

    add scripts to generate reference documentation for aspnet_compiler and installutil.

---
+++ reference/lib/dot_net_framework.rb
@@ -0,0 +1,29 @@
+class DotNetFramework
+
+  def initialize(path)
+    @path = path
+  end
+
+  def path
+    @path
+  end
+
+  def version
+    File.basename(@path).delete 'v'
+  end
+
+  def major_version
+    version.match(/^\d+/)[0]
+  end
+
+  def minor_version
+    version.match(/^\d+\.(\d+)/)[1]
+  end
+
+  def self.installations
+    win_dir = `echo %WINDIR%`.strip.gsub('\\', '/')
+    pattern = "#{win_dir}/Microsoft.Net/Framework/v*"
+    Dir[pattern].map { |dir| DotNetFramework.new dir }
+  end
+
+end
