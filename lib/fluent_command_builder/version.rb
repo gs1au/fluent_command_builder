40d9665f05726c8599d58a65b71400530d6588cf
Matthew Riley <matthew@matthewriley.name>
2012-08-16 22:08:47 +1000

    new version class to ultimately replace util class

---
+++ lib/fluent_command_builder/version.rb
@@ -0,0 +1,37 @@
+module FluentCommandBuilder
+  class Version
+
+    def initialize(version)
+      raise "#{version} is not a valid version." unless Version.is_valid? version
+      @version = version
+    end
+
+    def compact
+      @version.match(/^\d+\.\d+/)[0].sub('.', '')
+    end
+
+    def to_s
+      @version
+    end
+
+    def self.is_valid?(version)
+      version =~ /^(?:\d+\.)+(?:\d+)$/
+    end
+
+    def self.match(value)
+      version = value.match(/(?:\d+\.)+(?:\d+)/) { |m| m[0] }
+      version ? Version.new(version) : nil
+    end
+  end
+
+  def version_module(command_module)
+    version = command_module.respond_to?(:version) ? command_module.version : nil
+    raise "Unable to determine version for #{command_module::COMMAND_NAME}." unless version
+    module_name = "#{command_module.name}::V#{compact_version version}"
+    begin
+      eval module_name
+    rescue
+      raise "Unable to load #{module_name}. Please consider contributing."
+    end
+  end
+end
