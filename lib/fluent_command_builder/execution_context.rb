7e29e154265c6c4d0b9909c8afd5710835e7d49b
Matthew Riley <matthew@matthewriley.name>
2012-08-26 20:01:10 +1000

    print command in magenta

--- lib/fluent_command_builder/execution_context.rb
+++ lib/fluent_command_builder/execution_context.rb
@@ -1,8 +1,10 @@
+require 'term/ansicolor'
 require File.expand_path(File.dirname(__FILE__) + '/command_executors/system_executor')
 require File.expand_path(File.dirname(__FILE__) + '/command_formatters/null_formatter')
 
 module FluentCommandBuilder
   class ExecutionContext
+    include Term::ANSIColor
 
     attr_accessor :executor, :formatter, :should_print_on_execute, :should_fail_on_error
 
@@ -17,13 +19,17 @@ module FluentCommandBuilder
       validate_should_print_on_execute
       validate_should_fail_on_error
       visible_command = @formatter.format underlying_builder
-      puts visible_command if @should_print_on_execute && !@executor.will_print_on_execute?
+      print_command_on_execute(visible_command)
       @executor.execute(underlying_builder) do |status|
         raise "Command failed with status (#{status.exitstatus}): [#{visible_command}]" if @should_fail_on_error && !status.success?
       end
     end
 
     private
+
+    def print_command_on_execute(visible_command)
+      print magenta, visible_command, reset, "\n" if @should_print_on_execute && !@executor.will_print_on_execute?
+    end
 
     def validate_should_print_on_execute
       raise "should_print_on_execute must be true for #{@executor}." if !@should_print_on_execute && @executor.will_print_on_execute?
