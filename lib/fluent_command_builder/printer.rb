2e2631996c5ecadc965539273649991648f9cbbd
Matthew Riley <matthew@matthewriley.name>
2012-09-22 23:15:22 +1000

    add tests for printer

--- lib/fluent_command_builder/printer.rb
+++ lib/fluent_command_builder/printer.rb
@@ -5,18 +5,18 @@ module FluentCommandBuilder
     include Term::ANSIColor
 
     def print_warning(message)
-      STDERR.print yellow, 'Warning: ', message, reset, "\n"
-      STDERR.flush
+      $stderr.print yellow, 'Warning: ', message, reset, "\n"
+      $stderr.flush
     end
 
     def print_error(message)
-      STDERR.print red, 'Error: ', message, reset, "\n"
-      STDERR.flush
+      $stderr.print red, 'Error: ', message, reset, "\n"
+      $stderr.flush
     end
 
     def print_command(command)
       print magenta, command, reset, "\n"
-      STDOUT.flush
+      $stdout.flush
     end
 
   end
---
+++ test/lib/fluent_command_builder/printer_test.rb
@@ -0,0 +1,28 @@
+require 'test/unit'
+require_relative '../../../lib/fluent_command_builder'
+include FluentCommandBuilder
+
+class PrinterTest < Test::Unit::TestCase
+
+  def test_should_print_warning_in_yellow
+    $stderr = StringIO.new
+    printer = Printer.new
+    printer.print_warning 'This is a warning message.'
+    assert_equal "\e[33mWarning: This is a warning message.\e[0m\n", $stderr.string
+  end
+
+  def test_should_print_error_in_red
+    $stderr = StringIO.new
+    printer = Printer.new
+    printer.print_error 'This is an error message.'
+    assert_equal "\e[31mError: This is an error message.\e[0m\n", $stderr.string
+  end
+
+  def test_should_print_command_in_magenta
+    $stdout = StringIO.new
+    printer = Printer.new
+    printer.print_command 'command'
+    assert_equal "\e[35mcommand\e[0m\n", $stdout.string
+  end
+
+end
