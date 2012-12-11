c4e599dec6a7f2f2965b379d12a7b851a91897d0
Matthew Riley <matthew@matthewriley.name>
2012-12-11 19:25:21 +1100

    add password formatter to backticks executor

--- lib/fluent_command_builder/command_executors/backticks_executor.rb
+++ lib/fluent_command_builder/command_executors/backticks_executor.rb
@@ -3,8 +3,16 @@ require File.expand_path(File.dirname(__FILE__) + '/executor_base')
 module FluentCommandBuilder
   class BackticksExecutor < ExecutorBase
 
+    attr_accessor :password_formatter, :passwords
+
+    def initialize
+      @password_formatter = FluentCommandBuilder.password_formatter
+      @passwords = []
+    end
+
     def do_execute(command)
-      `#{command}`
+      output = `#{command}`
+      @password_formatter.format output, @passwords
     end
 
   end
