20ebf619a88aff56434d874396db94e9d8ba922c
Matthew Riley <matthew@matthewriley.name>
2013-08-03 16:15:00 +1000

    simplify path class

--- lib/fluent_command_builder/internal/path.rb
+++ lib/fluent_command_builder/internal/path.rb
@@ -1,26 +1,32 @@
 module FluentCommandBuilder
   class Path
 
-    attr_accessor :path, :is_windows
+    attr_accessor :path
 
     def initialize(*path)
       path.select! { |p| p unless p == '' }
       @path = File.join path
-      @is_windows = !ENV['WINDIR'].nil?
     end
 
     def evaluated_path
       return unless @path
+      # evaluates variables in path e.g. %WINDIR%
       `echo #{normalised_path}`.strip
     end
 
     def normalised_path
       return unless @path
-      @is_windows ? @path.gsub('/', '\\') : @path.gsub('\\', '/')
+      windows? ? @path.gsub('/', '\\') : @path.gsub('\\', '/')
     end
 
     def to_s
       evaluated_path
+    end
+
+    private
+
+    def windows?
+      !ENV['WINDIR'].nil?
     end
 
   end
--- test/lib/fluent_command_builder/internal/path_test.rb
+++ test/lib/fluent_command_builder/internal/path_test.rb
@@ -11,29 +11,31 @@ class PathTest < Test::Unit::TestCase
   end
 
   def test_should_normalise_path_on_unix
+    ENV['WINDIR'] = nil
     p = Path.new '\\path'
-    p.is_windows = false
     assert_equal '/path', p.normalised_path
   end
 
   def test_should_normalise_path_on_windows
+    ENV['WINDIR'] = 'C:\\Windows'
     p = Path.new 'C:/path'
-    p.is_windows = true
     assert_equal 'C:\\path', p.normalised_path
   end
 
-  def test_should_evaluate_path_on_unix
-    p = Path.new '$command_path'
-    return if p.is_windows
-    ENV['command_path'] = '/path'
-    assert_equal '/path', p.evaluated_path
+  unless ENV['WINDIR']
+    def test_should_evaluate_path_on_unix
+      p = Path.new '$command_path'
+      ENV['command_path'] = '/path'
+      assert_equal '/path', p.evaluated_path
+    end
   end
 
-  def test_should_evaluate_path_on_windows
-    p = Path.new '%command_path%'
-    return unless p.is_windows
-    ENV['command_path'] = 'C:\\path'
-    assert_equal 'C:\\path', p.evaluated_path
+  if ENV['WINDIR']
+    def test_should_evaluate_path_on_windows
+      p = Path.new '%command_path%'
+      ENV['command_path'] = 'C:\\path'
+      assert_equal 'C:\\path', p.evaluated_path
+    end
   end
 
   def test_should_ignore_empty_path_entries
